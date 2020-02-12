ffebca35e6cbf2b97af97b6b99729141ac271848
sbr1nkmann <github@brinkmann-nrw.de>
2020-02-12 22:30:59 +0100

    changes

--- app.py
+++ app.py
@@ -14,6 +14,17 @@ def motion_off():
     os.system("xscreensaver-command -activate")
     return jsonify(result='ok')
 
+@app.route('/api/monitor/on')
+def monitor_on():
+    os.system("vcgencmd display_power 1")
+    os.system("xscreensaver-command -deactivate")
+    return jsonify(result='ok')
+
+@app.route('/api/monitor/off')
+def monitor_off():
+    os.system("vcgencmd display_power 0")
+    return jsonify(result='ok')
+
 if __name__ == '__main__':
     app.run(debug=False, port=8080, host="0.0.0.0")
 
